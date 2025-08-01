b04da9b908426891dbec4f0c590d5ebb47e72e70
Nathan R <moso.nate.r@gmail.com>
2025-07-31 23:35:23 -0700

    Customizations

--- pages/user/mythbot.vue
+++ pages/user/mythbot.vue
@@ -1,9 +1,29 @@
 <template>
-  <MythbotMythbot />
+  <div class="grid h-full w-full gap-4">
+    <div>
+      <MythbotMythbot />
+    </div>
+    <div class="w-full gap-4 border dark:border-gray-800 border-gray-200 rounded-md p-4">
+      <NuxtPage />
+    </div>
+  </div>
 </template>
 
 <script setup>
 definePageMeta({
   layout: "user",
 });
+</script>
+<script>
+export default {
+  data() {
+    return {
+      ui: {
+        open: {
+          sideDraw: false,
+        },
+      },
+    };
+  },
+};
 </script>
